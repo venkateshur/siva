29ceddad4d6b45a579572a67b89d696359e09cb9
venkateshur <40040164+venkateshur@users.noreply.github.com>
2021-07-20 18:16:21 +0530

    Update FileTransferProcesstr.py

--- integration/model/processor/FileTransferProcesstr.py
+++ integration/model/processor/FileTransferProcesstr.py
@@ -1,12 +1,11 @@
-from paramiko import SSHClient
-from scp import SCPClient
+import os
+import subprocess
+
 
 def unix_transfer(path, host, port, user_name, password, input_file, output_file):
-    ssh = SSHClient()
-    ssh.load_system_host_keys()
-    ssh.connect(user_name + ":" + password + "@" + host + ":" + port + ":" + path)
+    p = subprocess.Popen(["scp", input_file, user_name + ":" + password + "@" + host + ":" + port + ":" + path + "/" + output_file])
+    return os.waitpid(p.pid, 0)
 
-    with SCPClient(ssh.get_transport()) as scp:
-        scp.put(input_file, output_file)
+
 
 
