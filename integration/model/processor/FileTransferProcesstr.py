d33f18ea02e272a3f6375d8098cd980639fc905d
venkateshur <40040164+venkateshur@users.noreply.github.com>
2021-08-17 20:09:53 +0530

    Update FileTransferProcesstr.py

--- integration/model/processor/FileTransferProcesstr.py
+++ integration/model/processor/FileTransferProcesstr.py
@@ -1,19 +1,30 @@
-import os
-import subprocess
 from ftplib import FTP
 from pathlib import Path
-
-
-#def unix_transfer(path, host, port, user_name, password, input_file, output_file):
-#    p = subprocess.Popen(["scp", input_file, user_name + ":" + password + "@" + host + ":" + port + ":" + path + "/" + output_file])
-#    return os.waitpid(p.pid, 0)
+from contextlib import contextmanager
+import shutil
+import os
 
 
 def unix_transfer(path, host, port, user_name, password, input_file):
     file_path = Path(input_file)
-    with FTP(host + ":" + port + ":" + path + "/", user_name, password) as ftp, open(file_path, 'rb') as file:
+    with FTP(host + ":" + port, user_name, password) as ftp, open(file_path, 'rb') as file:
+        ftp.dir(path)
         ftp.storbinary(f'STOR {file_path.name}', file)
 
 
+@contextmanager
+def network_share_auth(share, drive_letter, username=None, password=None):
+    cmd_parts = ["smbmount %s %s" % (share, drive_letter)]
+
+    if password:
+        cmd_parts.append("-o password=%s,username=%s" % (password, username))
+    os.system(" ".join(cmd_parts))
+    try:
+        yield
+    finally:
+        os.system("umount %s" % drive_letter)
 
 
+def windows_share(computer_share_name, file_name, username, password, drive_letter):
+    with network_share_auth(r"//" + computer_share_name, username, password):
+        shutil.copyfile(file_name, r"{0}".format(drive_letter))
