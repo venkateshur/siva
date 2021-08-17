2ed9dd59dc0e5a6104d26607fbfe787d32bddabe
venkateshur <40040164+venkateshur@users.noreply.github.com>
2021-08-17 20:08:39 +0530

    Update writer.py

--- integration/model/write/writer.py
+++ integration/model/write/writer.py
@@ -2,20 +2,21 @@ import cx_Oracle
 
 
 def load_into_oracle(data, oracle_conf):
-    con = cx_Oracle.connect(oracle_conf.hos)
+    conn = cx_Oracle.connect(oracle_conf.user_name, oracle_conf.password,
+                             oracle_conf.host + ":" + oracle_conf.port + "/" + oracle_conf.service_name, encoding="UTF-8")
     # cur = con.cursor()
-    data.toSql(oracle_conf.table_name, con)
+    data.toSql(oracle_conf.table_name, conn)
 
 
 def csv_writer(df, file_name, output_file_conf):
     if isinstance(df, dict):
         for sheet in df:
-            sheet.to_csv(output_file_conf.path + file_name + "_" + sheet + ".csv",
+            sheet.to_csv(output_file_conf.source_folder + file_name + "_" + sheet + ".csv",
                          index=None,
                          sep=output_file_conf.delimiter,
                          header=True)
     else:
-        df.to_csv(output_file_conf.path + file_name + ".csv",
+        df.to_csv(output_file_conf.source_folder + file_name + ".csv",
                   index=None,
                   sep=output_file_conf.delimiter,
                   header=True)
@@ -24,12 +25,17 @@ def csv_writer(df, file_name, output_file_conf):
 def text_writer(df, file_name, output_file_conf):
     if isinstance(df, dict):
         for sheet in df:
-            sheet.to_csv(output_file_conf.path + file_name + "_" + sheet + ".txt",
+            sheet.to_csv(output_file_conf.source_folder + file_name + "_" + sheet + ".txt",
                          index=None,
                          sep=output_file_conf.delimiter,
                          header=False)
     else:
-        df.to_csv(output_file_conf.path + file_name + ".txt",
+        df.to_csv(output_file_conf.source_folder + file_name + ".txt",
                   index=None,
                   sep=output_file_conf.delimiter,
                   header=False)
+
+
+def excel_writer(df, file_name, output_file_conf):
+    df.to_excel(output_file_conf.source_folder + file_name + ".xlsx",
+                header=True)
