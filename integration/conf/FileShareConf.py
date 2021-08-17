3fe39f0de2be9bdf632f94206392094a9dfdd9e7
venkateshur <40040164+venkateshur@users.noreply.github.com>
2021-08-17 20:13:30 +0530

    Update FileShareConf.py

--- integration/conf/FileShareConf.py
+++ integration/conf/FileShareConf.py
@@ -1,8 +1,10 @@
 class FileShareConf(object):
-    def __init__(self, system_type, host, port, user_name, password, path):
-        self.system_type = system_type
+    def __init__(self, system, host, port, user_name, password, source_folder, target_folder, file_name):
+        self.system = system
         self.host = host
         self.port = port
         self.user_name = user_name
         self.password = password
-        self.path = path
+        self.source_folder = source_folder
+        self.target_folder = target_folder
+        self.file_name = file_name
