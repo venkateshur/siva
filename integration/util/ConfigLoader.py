f75e0c81dceb2d4e73dc845740e622ed52f8bf95
venkateshur <40040164+venkateshur@users.noreply.github.com>
2021-08-27 22:41:11 +0530

    Update ConfigLoader.py

--- integration/util/ConfigLoader.py
+++ integration/util/ConfigLoader.py
@@ -1,24 +1,39 @@
-from conf.SharepointConf import SharepointConf
-from conf.FileConf import FileConf
-from conf.OracleConf import OracleConf
-from conf.InputConf import InputConf
-from conf.OutputConf import OutputConf
-from conf.FileShareConf import FileShareConf
-
-from conf.AppConf import AppConf
-
 import configparser
 
+from conf.AppConf import AppConf
+from conf.FileConf import FileConf
+from conf.FileConversionConf import FileConversionConf
+from conf.FileShareConf import FileShareConf
+from conf.FileTransferConf import FileTransferConf
+from conf.InputConf import InputConf
+from conf.UnixConf import UnixConf
+from conf.OracleConf import OracleConf
+from conf.OracleFileConf import OracleFileConf
+from conf.OutputConf import OutputConf
+from conf.SharepointConf import SharepointConf
+from conf.WindowsConf import WindowsConf
+from conf.RestServiceFIleConf import RestServiceFileConf
+from conf.RestConf import RestConf
 
-def load_app_config(config_path):
+
+def load_app_config(config_path, conf_type=None):
     try:
         conf = configparser.ConfigParser()
         conf.read(config_path)
         print(conf)
-        load_io_conf = load_conf(conf)
-        app_conf = get_app_conf(load_io_conf[0], load_io_conf[1], conf['CONVERSION']["type"])
+        if conf_type == "oracle-file":
+            config = oracle_file_conf(conf)
+        elif conf_type == "file-conversion":
+            config = file_conversion_conf(conf)
+        elif conf_type == "file-transfer":
+            config = file_transfer_conf(conf)
+        elif conf_type == "rest-service-file":
+            config = rest_service_file_conf(conf)
+        else:
+            load_io_conf = load_conf(conf)
+            config = get_app_conf(load_io_conf[0], load_io_conf[1], conf['CONVERSION']["type"])
 
-        return app_conf
+        return config
 
     except Exception as e:
         raise e
@@ -76,3 +91,75 @@ def load_conf(config):
     output_conf = OutputConf(target, oracle_conf, output_file_conf, file_share_conf)
 
     return input_conf, output_conf
+
+
+def file_transfer_conf(config):
+    linux_conf = UnixConf(config['FILE-TRANSFER.UNIX']['cert_directory'],
+                          config['FILE-TRANSFER.UNIX']['cert_name'],
+                          config['FILE-TRANSFER.UNIX']['cert_password'],
+                          config['FILE-TRANSFER.UNIX']['server'],
+                          config['FILE-TRANSFER.UNIX']['file_path'])
+
+    windows_conf = WindowsConf(config['FILE-TRANSFER.WINDOWS']['dir'])
+    return FileTransferConf(config['FILE-TRANSFER']['source_system'],
+                            config['FILE-TRANSFER']['target_system'],
+                            linux_conf,
+                            windows_conf)
+
+
+def file_conversion_conf(config):
+    source_file_conf = FileConf(config['FILE-CONVERSION.SOURCE-FILE']['path'],
+                                config['FILE-CONVERSION.SOURCE-FILE']['file_format'],
+                                config['FILE-CONVERSION.SOURCE-FILE']['excel_all_sheets'],
+                                config['FILE-CONVERSION.SOURCE-FILE']['excel_required_sheets'],
+                                config['FILE-CONVERSION.SOURCE-FILE']['header_fields'])
+
+    target_file_conf = FileConf(config['FILE-CONVERSION.TARGET-FILE']['path'],
+                                config['FILE-CONVERSION.TARGET-FILE']['file_format'],
+                                config['FILE-CONVERSION.TARGET-FILE']['excel_all_sheets'],
+                                config['FILE-CONVERSION.TARGET-FILE']['excel_required_sheets'],
+                                config['FILE-CONVERSION.TARGET-FILE']['header_fields'])
+
+    return FileConversionConf(config['FILE-CONVERSION']['source_format'],
+                              config['FILE-CONVERSION']['target_format'],
+                              source_file_conf,
+                              target_file_conf)
+
+
+def oracle_file_conf(config):
+    oracle_conf = OracleConf(config['ORACLE-FILE.ORACLE']['host'],
+                             config['ORACLE-FILE.ORACLE']['port'],
+                             config['ORACLE-FILE.ORACLE']['service_name'],
+                             config['ORACLE-FILE.ORACLE']['user_name'],
+                             config['ORACLE-FILE.ORACLE']['password'],
+                             config['ORACLE-FILE.ORACLE']['table_name'],
+                             config['ORACLE-FILE.ORACLE']['columns'].split(";"))
+
+    file_conf = FileConf(config['ORACLE-FILE.ORACLE']['path'],
+                         config['ORACLE-FILE.ORACLE']['file_format'],
+                         config['ORACLE-FILE.ORACLE']['excel_all_sheets'],
+                         config['ORACLE-FILE.ORACLE']['excel_required_sheets'].split(";"),
+                         config['ORACLE-FILE.ORACLE']['header_fields'].split(";"))
+
+    return OracleFileConf(config['ORACLE-FILE']['source'],
+                          config['ORACLE-FILE']['target'],
+                          oracle_conf,
+                          file_conf)
+
+
+def rest_service_file_conf(config):
+    rest_conf = RestConf(config['REST-SERVICE-FILE.REST']['api'],
+                         config['REST-SERVICE-FILE.REST']['cert_directory'],
+                         config['REST-SERVICE-FILE.REST']['cert_name'],
+                         config['REST-SERVICE-FILE.REST']['cert_password'])
+
+    file_conf = FileConf(config['REST-SERVICE-FILE.FILE']['path'],
+                         config['REST-SERVICE-FILE.FILE']['file_format'],
+                         config['REST-SERVICE-FILE.FILE']['excel_all_sheets'],
+                         config['REST-SERVICE-FILE.FILE']['excel_required_sheets'],
+                         config['REST-SERVICE-FILE.FILE']['header_fields'])
+
+    return RestServiceFileConf(config['REST-SERVICE-FILE']['source'],
+                               config['REST-SERVICE-FILE']['target'],
+                               rest_conf,
+                               file_conf)
