358441b1ad12cb7e2b98b7388efb77cca3d438fb
venkateshur <40040164+venkateshur@users.noreply.github.com>
2021-08-31 19:04:32 +0530

    Update OracleFileIntegration.py

--- integration/OracleFileIntegration.py
+++ integration/OracleFileIntegration.py
@@ -2,16 +2,17 @@ import logging
 import sys
 from datetime import datetime
 
-from model.processor.MainProcessor import csv_processor, excel_processor, text_processor
 from model.read import Reader
 from model.write import writer
 from model.write.writer import load_into_oracle
-from util.ConfigLoader import load_app_config
+from conf.OracleConf import OracleConf
+from conf.FileConf import FileConf
+import glob
 
 if __name__ == "__main__":
-    print("Data integrator - Loading Of Oracle")
+    print("Data integrator - Oracle File Loading or Unloading")
 
-logger = logging.getLogger("Data integrator - Loading Of Oracle")
+logger = logging.getLogger("Data integrator - Oracle File Loading or Unloading")
 logger.setLevel(logging.INFO)
 
 current_timestamp = str(datetime.now())
@@ -20,47 +21,30 @@ print('Loading file to oracle - Started @: ' + current_timestamp)
 
 try:
     load_type = sys.argv[1]
-    input_file_path = sys.argv[2]
-    table_name = sys.argv[3]
-    config_path = sys.argv[4]
+    file_path = sys.argv[2]
+    delimiter = sys.argv[3]
+    header = sys.argv[4]
+    required_fields = sys.argv[5]
 
-    config = load_app_config(config_path, "oracle-file")
+    table_name = sys.argv[6]
+    host = sys.argv[7]
+    port = sys.argv[8]
+    service_name = sys.argv[9]
+    user_name = sys.argv[10]
+    password = sys.argv[11]
+    columns = sys.argv[12]
+
+    oracle_conf = OracleConf(host, port, service_name, user_name, password, table_name, columns)
     if load_type == "FILE-ORACLE":
-        if config.file_conf.file_format == "CSV":
-            data = csv_processor(config.file_conf.path,
-                                 config.file_conf.fields,
-                                 config.file_conf.delimiter,
-                                 config.file_conf.header)
-        elif config.file_conf.file_format == "EXCEL":
-            data = excel_processor(config.file_conf.path,
-                                   config.file_conf.all_sheets_required,
-                                   config.file_conf.required_sheets,
-                                   config.file_conf.header)
+        files = glob.glob(file_path + "/*.csv")
+        for file in files:
+            data = Reader.csv_reader(file, required_fields, delimiter, header)
+            load_into_oracle(data, oracle_conf)
 
-        elif config.file_conf.file_format == "TEXT":
-            data = text_processor(config.file_conf.path,
-                                  config.file_conf.delimiter)
-        else:
-            raise Exception("Invalid file format, supported file formats are CSV, EXCEL and TEXT")
-
-        if isinstance(data, dict):
-            for df in data:
-                load_into_oracle(df, config.output_conf.oracle_conf)
-        else:
-            load_into_oracle(data, config.output_conf.oracle_conf)
-
-    if config.source == "ORACLE-FILE":
-        data = Reader.read_from_oracle_query(config.oracle_conf)
-        if config.file_conf.file_format == "CSV":
-            writer.csv_writer(data, config.oracle_conf.table_name.replace(".", "_"),
-                              config.output_conf.file_conf)
-        elif config.file_conf.file_format == "TEXT":
-            writer.text_writer(data, config.oracle_conf.table_name.replace(".", "_"),
-                               config.output_conf.file_conf)
-        elif config.file_conf.file_format == "EXCEL":
-            writer.excel_writer(data, config.oracle_conf.table_name.replace(".", "_"), config.file_conf)
-        else:
-            raise Exception("Invalid File Format, Supported File formats are CSV, Text and Excel")
+    if load_type == "ORACLE-FILE":
+        data = Reader.read_from_oracle_query(oracle_conf)
+        file_conf = FileConf(file_path, "CSV", None, None, None, True)
+        writer.csv_writer(data, oracle_conf.table_name.replace(".", "_"), file_conf)
 
 except Exception as e:
     logger.error("Loading file to oracle Failed with Error:" + str(e))
